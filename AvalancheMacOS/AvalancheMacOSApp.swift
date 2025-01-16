e1cf027757202eba9564824edc91e0b967dcd49a
Pierce Boggan <piboggan@microsoft.com>
2025-01-16 13:46:59 -0600

    Add macOS menu bar app

---
+++ AvalancheMacOS/AvalancheMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvalancheMacOSApp.swift
+//  AvalancheMacOS
+//
+//  Created by Pierce Boggan on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AvalancheMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
